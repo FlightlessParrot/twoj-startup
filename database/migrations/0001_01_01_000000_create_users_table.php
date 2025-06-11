97488b27320d76969b99878de26e6d7d7f7051d4
Konrad Strauss <shrimpinweb@gmail.com>
2025-06-11 20:02:33 +0200

    user full data in migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');;
+            $table->string('last_name');
             $table->string('phone')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
